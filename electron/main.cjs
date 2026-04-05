7ffc4b0d639887be207e7f61dd46a900275dd90d
Guanghua Xiao <xiaofamily@Guanghuas-Mac-mini.local>
2026-04-04 22:07:06 -0500

    Add Electron desktop app support

---
+++ electron/main.cjs
@@ -0,0 +1,43 @@
+const { app, BrowserWindow, shell } = require('electron');
+const path = require('path');
+
+const isDev = process.env.NODE_ENV === 'development';
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 900,
+    height: 700,
+    minWidth: 600,
+    minHeight: 500,
+    titleBarStyle: 'hiddenInset',
+    title: 'FlashCard AI',
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  if (isDev) {
+    win.loadURL('http://localhost:5173');
+  } else {
+    win.loadFile(path.join(__dirname, '../dist/index.html'));
+  }
+
+  // Open external links in default browser instead of Electron
+  win.webContents.setWindowOpenHandler(({ url }) => {
+    shell.openExternal(url);
+    return { action: 'deny' };
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
